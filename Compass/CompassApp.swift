3f1a34aa43abc2fa59c84a4babe324aa7faf997c
fujimori <fujimori@suredesigns.com>
2021-06-02 18:03:46 +0900

    Initial Commit

---
+++ Compass/CompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompassApp.swift
+//  Compass
+//
+//  Created by Strigops habroptilus on 2021/06/02.
+//
+
+import SwiftUI
+
+@main
+struct CompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Compass/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Compass
+//
+//  Created by Strigops habroptilus on 2021/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
